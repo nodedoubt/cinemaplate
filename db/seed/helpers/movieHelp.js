f2376b8b5a16248b39a1a8abac97e72559e20049
Brad Fincher <matrixv01@gmail.com>
2016-02-24 10:40:25 -0600

    Updated api query format

--- db/seed/helpers/movieHelp.js
+++ db/seed/helpers/movieHelp.js
@@ -28,9 +28,8 @@ movie.getMovieDB = function(redditObj){
         var escaped = escape(arr[x].title);
         allCalls.push(rp('http://api.themoviedb.org/3/search/movie?query=' + escaped + '&api_key=' + process.env.MOVIEDB_TOKEN))
         // TODO change moviedb api to query for movie id instead of title query
-        // allCalls.push(rp('http://api.themoviedb.org/3/search/movie?query=' + escaped + '&api_key=' + process.env.MOVIEDB_TOKEN))
-        'http://api.themoviedb.org/3/movie/' + currMovie.id + '?api_key=' + process.env.MOVIEDB_TOKEN + '&append_to_response=videos'
-        // 'http://api.themoviedb.org/3/movie/157336?api_key=52bc5e6536cc2a2bbe8cf52f7395ef0b&append_to_response=videos'
+        // allCalls.push(rp(''http://api.themoviedb.org/3/movie/' + currMovie.id + '?api_key=' + process.env.MOVIEDB_TOKEN + '&append_to_response=videos'))
+        // example for Interstellar: 'http://api.themoviedb.org/3/movie/157336?api_key=52bc5e6536cc2a2bbe8cf52f7395ef0b&append_to_response=videos'
 
     }
 
