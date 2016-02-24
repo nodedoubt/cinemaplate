edbb79ae708c9c9d163cb8a18e5a7de32c97649b
Julia Brown <juliacelestebrown@gmail.com>
2016-02-23 19:48:25 -0600

    yelp now showing up

--- server/db.js
+++ server/db.js
@@ -4,18 +4,11 @@ var pg = require('pg')
 // Get Postgres rolling.
 //
 var pgConString = '';
-var pgConConfig = {
+exports.pgConConfig = {
   database: "cinemaplate_dev",
   host: "localhost",
   port: 5432
 }
-module.exports = pgClient
-
-var pgClient = new pg.Client(pgConConfig);	
 
 
-pgClient.on('drain', function() {
-  pgClient.end();
-});
 
-pgClient.connect()
